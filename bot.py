6ef7a1daa50a7fcc4841e9b2df9ee67c74b95176
MD-Developer-yt <animeworldday2005@gmail.com>
2026-03-26 23:53:26 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,94 @@
+import os
+import time
+import pyromod.listen
+from datetime import datetime, timedelta
+from pytz import timezone
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
+from config import Config
+from aiohttp import web
+from route import web_server
+import pyrogram.utils
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+# ----------------------------------------
+
+#Don't Remove Credits
+#Ask Doubt @AU_Bot_Discussion 
+#Telegram Channel @Anime_UpdatesAU 
+#Owner @Mr_Mohammed_29
+# --
+pyrogram.utils.MIN_CHANNEL_ID = -1002964099736
+# ----------------------------------------
+
+#Don't Remove Credits
+#Ask Doubt @AU_Bot_Discussion 
+#Telegram Channel @Anime_UpdatesAU 
+#Owner @Mr_Mohammed_29
+# --
+PORT = Config.PORT
+# ----------------------------------------
+
+#Don't Remove Credits
+#Ask Doubt @AU_Bot_Discussion 
+#Telegram Channel @Anime_UpdatesAU 
+#Owner @Mr_Mohammed_29
+# --
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="rexbots",
+            api_id=Config.API_ID,
+            api_hash=Config.API_HASH,
+            bot_token=Config.BOT_TOKEN,
+            workers=200,
+            plugins={"root": "plugins"},
+            sleep_threshold=15,
+        )
+        self.start_time = time.time()
+# ----------------------------------------
+
+#Don't Remove Credits
+#Ask Doubt @AU_Bot_Discussion 
+#Telegram Channel @Anime_UpdatesAU 
+#Owner @Mr_Mohammed_29
+# --
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.mention = me.mention
+        self.username = me.username
+        self.uptime = Config.BOT_UPTIME
+        if Config.WEBHOOK:
+            app = web.AppRunner(await web_server())
+            await app.setup()
+            await web.TCPSite(app, "0.0.0.0", PORT).start()
+        print(f"{me.first_name} Is Started.....✨️")
+        uptime_seconds = int(time.time() - self.start_time)
+        uptime_string = str(timedelta(seconds=uptime_seconds))
+        for chat_id in [Config.LOG_CHANNEL, Config.SUPPORT_CHAT]:
+            try:
+                curr = datetime.now(timezone("Asia/Kolkata"))
+                date = curr.strftime('%d %B, %Y')
+                time_str = curr.strftime('%I:%M:%S %p')
+                await self.send_photo(
+                    chat_id=chat_id,
+                    photo=Config.START_PIC,
+                    caption=(
+                        "**I ʀᴇsᴛᴀʀᴛᴇᴅ ᴀɢᴀɪɴ !**\n\n"
+                        f"ɪ ᴅɪᴅɴ'ᴛ sʟᴇᴘᴛ sɪɴᴄᴇ​: `{uptime_string}`"
+                    ),
+                    reply_markup=InlineKeyboardMarkup(
+                        [[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/Anime_UpdatesAU")]]
+                    )
+                )
+            except Exception as e:
+                print(f"Failed to send message in chat {chat_id}: {e}")
+
+Bot().run()
+# ----------------------------------------
+
+#Don't Remove Credits
+#Ask Doubt @AU_Bot_Discussion 
+#Telegram Channel @Anime_UpdatesAU 
+#Owner @Mr_Mohammed_29
+# --
